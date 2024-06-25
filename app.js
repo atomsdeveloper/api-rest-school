902ec5e1173ee80354646e2ef1d5c8627ca92226
renanns <atomsfrontenf@gmail.com>
2024-06-25 11:24:43 -0300

    feat: add app.js with all configs to render applications

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
+import { resolve } from 'path';
+
+import cors from 'cors';
+import helmet from 'helmet';
 
 import HomeRoutes from './src/routes/HomeRoutes.js';
 import UserRoutes from './src/routes/UserRoutes.js';
 import TokenRoutes from './src/routes/TokenRoutes.js';
+import AlunoRoutes from './src/routes/AlunoRoutes.js';
+import PhotoRoutes from './src/routes/PhotoRoutes.js';
 
 import './src/database/index.js';
+
+const whiteList = [
+  'https://escola.atomsdev.com.br',
+  'http://localhost:3000',
+]
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowesd by cors'))
+    }
+  }
+}
 
 class App {
   constructor() {
@@ -17,14 +35,19 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(process.env.DIRNAME, 'uploads', 'images')));
   }
 
   routes() {
     this.app.use('/', HomeRoutes);
     this.app.use('/users/', UserRoutes);
     this.app.use('/tokens/', TokenRoutes);
+    this.app.use('/alunos/', AlunoRoutes);
+    this.app.use('/photos/', PhotoRoutes);
   }
 }
 

1ee8f64fb2552888aa34880a62214e5d157a715d
renanns <atomsfrontenf@gmail.com>
2024-06-25 11:32:09 -0300

    chore: created file to migrations data to database with postegresql

---
+++ src/database/migrations/20240612144852-alunos.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return await queryInterface.createTable('alunos', {
+      // Campo auto
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      // Campos autos
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return await queryInterface.dropTable('alunos');
+  },
+};

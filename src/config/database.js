e874f277935fe8b62ea8455e69ade35f35f9e1d4
renanns <atomsfrontenf@gmail.com>
2024-06-25 11:28:38 -0300

    chore: add file to config database

---
+++ src/config/database.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const database = {
+  dialect: 'mariadb',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezon: 'America/Sao_Paulo',
+};
+export default database;
